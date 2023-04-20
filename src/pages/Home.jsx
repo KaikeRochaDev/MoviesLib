64994858287aaf84fcfc801425378e4257afbc31
Kaike Rocha <dev.kaikerocha@gmail.com>
2023-04-20 15:05:44 -0300

    listing the movies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,30 @@
+import { useState, useEffect } from "react"
+
+const moviesURL = import.meta.env.VITE_API
+const apiKey = import.meta.env.VITE_API_KEY
+
 const Home = () => {
+  const [topMovies, setTopMovies] = useState([])
+
+  const getTopRateMovies = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    setTopMovies(data.results)
+  }
+
+  useEffect(() => {
+    const topRateurl = `${moviesURL}top_rated?${apiKey}`
+
+    getTopRateMovies(topRateurl)
+  }, [])
   return (
-    <div>Home</div>
+    <div className="container">
+      <h2 className="title">Melhores filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length > 0 && topMovies.map((movie) => <p>{movie.title}</p>)}
+      </div>
+    </div>
   )
 }
 
