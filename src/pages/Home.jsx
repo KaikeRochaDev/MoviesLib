686681286d30fd6f719685837157caff02ae62ae
Kaike Rocha <dev.kaikerocha@gmail.com>
2023-04-20 15:19:53 -0300

    Adding the movie images and notes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react"
+import MovieCard from "../components/MovieCard"
 
 const moviesURL = import.meta.env.VITE_API
 const apiKey = import.meta.env.VITE_API_KEY
@@ -22,7 +23,7 @@ const Home = () => {
     <div className="container">
       <h2 className="title">Melhores filmes:</h2>
       <div className="movies-container">
-        {topMovies.length > 0 && topMovies.map((movie) => <p>{movie.title}</p>)}
+        {topMovies.length > 0 && topMovies.map((movie) => <MovieCard movie={movie} key={movie.id} />)}
       </div>
     </div>
   )
